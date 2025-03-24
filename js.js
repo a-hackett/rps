954b651bd9ac89fba73bc0d3d83914d114c8515a
a-hackett <65570915+a-hackett@users.noreply.github.com>
2025-03-24 16:48:53 -0400

    updated js to increase opacity of box when mouseover

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
 const body = document.querySelector("body");
 body.classList.add("flex");
 
+const container = document.querySelector("#container");
+document.getElementById("container").style.order="3";
+container.classList.add("flex", "autoMargins");
+
 const title = document.createElement("h1");
 title.classList.add("autoMargins");
 title.id= "title";
@@ -12,24 +16,17 @@ const button = document.createElement("button");
 button.id = "button";
 button.classList.add("flex", "autoMargins");
 button.textContent=("Submit");
-button.addEventListener("click", ()=> {
-    console.log("here");
-    container.innerHTML='';
-    createBoxes(0);
-    });
 body.appendChild(button);
 document.getElementById("button").style.order="2";
 
 
-const container = document.querySelector("#container");
-document.getElementById("container").style.order="3";
-container.classList.add("flex", "autoMargins");
+button.addEventListener("click", ()=> {
+    container.innerHTML='';
+    size=prompt("enter number of boxes:");
+    createBoxes(size);
+});
 
 function createBoxes(size){
-    if(size==0){
-        console.log("in 0")
-        size=prompt("enter number of boxes:");
-    }
     if(size>0 && size<100){
         for (let i=0;i<size;i++){
             const row = document.createElement("div");
@@ -38,16 +35,22 @@ function createBoxes(size){
             for (let j=0;j<size;j++){
                 const box = document.createElement("div");
                 box.classList.add("box");
-                box.addEventListener('mouseover', () => {
+                row.append(box);
+
+                box.addEventListener('mouseover', () => {  
+                    // highlight the mouseover target
                     const randomHexColorCode = () => {
                         let n = (Math.random() * 0xfffff * 1000000).toString(16);
-                        return '#' + n.slice(0, 6);
-                      };
-                      
+                        return ('#' + n.slice(0, 6));
+                    };
+
                     box.style.backgroundColor = randomHexColorCode();
-                });
-                row.append(box);
-            }container.appendChild(row);
+                    box.style.opacity = (parseFloat(box.style.opacity) || 0) + 0.5;
+                  });
+
+                }container.appendChild(row);
         }
     }
-}button.click(true)
+}
+
+button.click(true)
